02c37f1e745fe4ebf7233b6724c4ce33ca747d9f
robin <robindanielk@gmail.com>
2017-08-12 16:56:18 +0530

    Youtube Intent

--- app/src/main/java/com/example/user/popularmoviesapp/Helper/Constants.java
+++ app/src/main/java/com/example/user/popularmoviesapp/Helper/Constants.java
@@ -4,10 +4,14 @@ package com.example.user.popularmoviesapp.Helper;
  * Created by user on 7/14/2017.
  */
 
+/*Helper Class for all the Constants*/
+
 public class Constants
 {
 
     public static final String MOVIE_INTENT_ID = "movies";
     public static final String MOVIE_SAVED_INSTANCE_KEY = "movie_saved_instance";
+    public static final String YOUTUBE_APP = "vnd.youtube:";
+    public static final String YOUTUBE_URL_BROWSER = "http://www.youtube.com/watch?v=";
 
 }
