fa1bcafbab26b232f6218b4827838cf7c254ec61
robin <robindanielk@gmail.com>
2017-08-23 18:21:48 +0530

    Favorites implementation

--- app/src/main/java/com/example/user/popularmoviesapp/database/MovieDbHelper.java
+++ app/src/main/java/com/example/user/popularmoviesapp/database/MovieDbHelper.java
@@ -23,14 +23,13 @@ public class MovieDbHelper extends SQLiteOpenHelper
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String CREATE_TABLE_MOVIES =
-                "CREATE TABLE " + MoviesEntry.TABLE_NAME + " (" +
+        final String CREATE_TABLE_MOVIES = "CREATE TABLE " + MoviesEntry.TABLE_NAME + " (" +
                         MoviesEntry.COLUMN_MOVIE_ID + " INTEGER PRIMARY KEY, " +
                         MoviesEntry.COLUMN_MOVIE_TITLE + " TEXT, "  +
-                        MoviesEntry.COLUMN_MOVIE_DURATION + " TEXT NOT, " +
+                        MoviesEntry.COLUMN_MOVIE_DURATION + " TEXT, " +
                         MoviesEntry.COLUMN_MOVIE_USER_RATING + " REAL, " +
                         MoviesEntry.COLUMN_MOVIE_BACKDROP + " TEXT, " +
-                        MoviesEntry.COLUMN_MOVIE_POSTER + " TEXT,"  +
+                        MoviesEntry.COLUMN_MOVIE_POSTER + " TEXT, "  +
                         MoviesEntry.COLUMN_MOVIE_RELEASE_DATE + " TEXT " + ")";
         db.execSQL(CREATE_TABLE_MOVIES);
     }
